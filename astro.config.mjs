bf1b71aa0829544ac4db3ffa9b7ba1e683a89ae9
MVidicek <mvidicek@protonmail.com>
2022-07-22 21:33:58 +0200

    Add home review & seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://konoba-jadra.netlify.app',
+  site: 'https://www.konoba-jadra.com',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Konoba Jadra | A Seafood Restaurant`,
-	description: `A cozy restaurant with a warm atmosphere located in Croatia on Kornati National Park.`,
-	url: `https://konoba-jadra.netlify.app`, // No trailing slash!
-	name: `Jadra`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	description: `A cozy restaurant with a warm atmosphere located in Kornati National Park, Croatia.`,
+	url: `https://www.konoba-jadra.com`, // No trailing slash!
+	name: `Konoba Jadra`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
 };
