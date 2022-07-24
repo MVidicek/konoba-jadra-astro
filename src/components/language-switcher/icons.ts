cd94b8a13830a514f699a31da60c1e008ca24882
MVidicek <mvidicek@protonmail.com>
2022-07-25 00:50:58 +0200

    Home css layout modified

--- src/components/language-switcher/icons.ts
+++
@@ -1,9 +0,0 @@
-import { html } from 'lit';
-
-export const classicThemeIcon = html`<img id="hr" src="/assets/images/countries/croatia.png" alt="croatian flag">`;
-
-export const earthThemeIcon = html`<img id="de" src="/assets/images/countries/germany.png" alt="german flag">`;
-
-export const blueThemeIcon = html`<img id="it" src="/assets/images/countries/italy.png" alt="italian flag">`;
-
-export const orangeThemeIcon = html`<img id="uk" src="/assets/images/countries/united-kingdom.png" alt="great britain flag">`;
