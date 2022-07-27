b4784e2bedcf660a0da93fa6f6b0a543b6ec30fe
MVidicek <mvidicek@protonmail.com>
2022-07-27 03:31:49 +0200

    SEO, Google Analytics, design fixes

--- src/components/theme-switcher/icons.ts
+++ src/components/theme-switcher/icons.ts
@@ -1,9 +1,9 @@
 import { html } from 'lit';
 
-export const classicThemeIcon = html`<img style="max-width: 64px;" id="hr" src="/assets/images/countries/croatia.png" alt="croatian flag">`;
+export const classicThemeIcon = html`<p>Test</p>`;
 
-export const earthThemeIcon = html`<img style="max-width: 64px;" id="de" src="/assets/images/countries/germany.png" alt="german flag">`;
+export const earthThemeIcon = html`<p>Test</p>`;
 
-export const blueThemeIcon = html`<img style="max-width: 64px;" id="it" src="/assets/images/countries/italy.png" alt="italian flag">`;
+export const blueThemeIcon = html`<p>Test</p>`;
 
-export const orangeThemeIcon = html`<img style="max-width: 64px;" id="uk" src="/assets/images/countries/united-kingdom.png" alt="great britain flag">`;
+export const orangeThemeIcon = html`<p>Test</p>`;
