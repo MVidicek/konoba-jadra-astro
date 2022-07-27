675566140b16cba9b5a1c19a1ca5086379c60825
MVidicek <mvidicek@protonmail.com>
2022-07-27 03:50:52 +0200

    More SEO

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Konoba Jadra | A Seafood Restaurant`,
-	description: `Konoba Jadra is a seafood restaurant located on Kornati National Park, Otok Piškera in the central region of Croatia. It is a great place to enjoy a meal with your friends or family.`,
+	description: `Konoba Jadra is a seafood restaurant located on Kornati National Park, Otok Piškera in the central region of Croatia. It is a great place to enjoy a meal with your friends or family. We offer a wide range of dishes from the best chefs in the region.`,
 	url: `www.konoba-jadra.com`, // No trailing slash!
 	name: `Konoba Jadra`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
