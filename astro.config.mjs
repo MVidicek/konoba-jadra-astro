5ffa562c510a02c026dd45604fc706c54de37303
MVidicek <mvidicek@protonmail.com>
2023-05-06 16:47:58 +0200

    Fix carousels

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.konoba-jadra.com',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true,
-  // Generate sitemap (set to "false" to disable)
-  integrations: [lit(), sitemap(), tailwind()],
-  // Add renderers to the config
-  vite: {
-    ssr: {
-      external: ["svgo"]
-    }
-  }
+	site: 'https://www.konoba-jadra.com',
+	// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+	sitemap: true,
+	// Generate sitemap (set to "false" to disable)
+	integrations: [lit(), sitemap(), tailwind()],
+	// Add renderers to the config
+	vite: {
+		ssr: {
+			external: ['svgo'],
+		},
+	},
 });
